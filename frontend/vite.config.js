a29bf2f66e0bf96cfe1bf000e8038e2341d89cfb
sameer-ahmed-2000 <sameersameersmr20@gmail.com>
2024-09-12 21:56:27 +0530

    updated package

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,11 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['@fortawesome/react-fontawesome', '@fortawesome/free-solid-svg-icons']
+      external: [
+        '@fortawesome/react-fontawesome',
+        '@fortawesome/free-solid-svg-icons',
+        '@fortawesome/free-brands-svg-icons'
+      ]
     }
   }
 });
